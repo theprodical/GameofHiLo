7b676b8323bc1cd98775a018a30ec6bef99b56ae
Jim Brumbaugh <doctordread@gmail.com>
2022-10-20 21:09:26 -0600

    Add project files.

---
+++ GameofHiLo/HiLoGame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GameofHiLo
+{
+    static class HiLoGame
+    {
+        public const int MAXIMUM = 10;
+        private static Random random = new Random();
+        private static int currentNumber = random.Next(1, MAXIMUM + 1);
+        private static int pot = 10;
+
+        public static int GetPot() { return pot; }
+
+        public static void Guess(bool higher)
+        {
+            int nextNumber = random.Next(1, MAXIMUM + 1);
+            if ((higher && nextNumber >= currentNumber) ||
+                (!higher && nextNumber <= currentNumber))
+            {
+                Console.WriteLine("You guessed right!!!");
+                pot++;
+            }
+            else
+            {
+                Console.WriteLine("Bad luck, you guessed WRONG!!!!");
+                pot--;
+            }
+            currentNumber = nextNumber;
+            Console.WriteLine($"The current number is {currentNumber}");
+        }
+
+        public static void Hint()
+        {
+            int half = MAXIMUM / 2;
+            if (currentNumber >= half)
+                Console.WriteLine($"The number is at least {half}");
+            else Console.WriteLine($"The number is at most {half}");
+            pot--;
+        }
+
+    }
+}
---
+++ GameofHiLo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameofHiLo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hi-Lo");
+            Console.WriteLine($"Guess numbers between 1 and {HiLoGame.MAXIMUM}.");
+            HiLoGame.Hint();
+
+            while (HiLoGame.GetPot() > 0)
+            {
+                Console.WriteLine("Press h for higher, l for lower, ? to buy a hint,");
+                Console.WriteLine($" or any other key to quit with {HiLoGame.GetPot()}. ");
+                char key = Console.ReadKey(true).KeyChar;
+                if (key == 'h') HiLoGame.Guess(true);
+                else if (key == 'l') HiLoGame.Guess(false);
+                else if (key == '?') HiLoGame.Hint();
+                else return;
+            }
+            Console.WriteLine("The pot is empty!!! Bye.");
+        }
+    }
+}
